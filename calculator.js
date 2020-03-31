55827ad70fb14b68dd1f8bbd7cf9e1f18c124bb5
dnguye2 <dnguyen1093@gmail.com>
2020-03-30 21:53:11 -0700

    plan in pseudocode

--- calculator.js
+++ calculator.js
@@ -3,3 +3,26 @@ const exampleAdditionInput = {
   num2: 5,
   operation: 'add',
 }
+
+// plan
+
+// if else statement
+// if operation === add || operation === +
+  // return num1 + num 2
+// elsif subtract
+// elsif multiply
+// elsif divide
+// else invalid operator
+  // print 'invalid operator'
+
+
+// handling edge cases
+// put before the calculator if else statements
+
+// user input is undefined
+// if num1 || num2 undefined, print error message
+
+// invalid operator
+// see above
+
+
